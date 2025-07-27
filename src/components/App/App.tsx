74fa996a021e5528819b564cf6e5b2c6e8f2be89
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-07-27 22:23:17 +0300

    add new files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,31 @@
+import css from "./App.module.css"
+import { useState } from 'react'
+import CafeInfo from "../CafeInfo/CafeInfo"
+import type { Votes, VoteType } from '../../types/votes'
+// import VoteOptionsProps from "../VoteOptions/VoteOptions"
+import VoteOptions from '../VoteOptions/VoteOptions';
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    })
+  }
+  const resetVotes = () => {
+    setVotes(
+      { good: 0, neutral: 0, bad: 0 }
+    )
+  }
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+    </div>
+  )
+}
+
+export default App
