5511f54ff67cdb2af99bf5fe378c75199e77dd8e
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-08-03 13:58:24 +0300

    add Notification and update VoteStates

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import css from "./App.module.css"
 import { useState } from 'react'
-import CafeInfo from "../CafeInfo/CafeInfo"
 import type { Votes, VoteType } from '../../types/votes'
-// import VoteOptionsProps from "../VoteOptions/VoteOptions"
+import CafeInfo from "../CafeInfo/CafeInfo"
 import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
@@ -20,10 +21,18 @@ function App() {
     )
   }
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
+      {totalVotes > 0
+      ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={totalVotes
+        ? Math.round((votes.good / totalVotes) * 100)
+        : 0}
+        />
+      : <Notification />}
     </div>
   )
 }
